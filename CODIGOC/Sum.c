8a42b7f0c60cd0df2a3a957c3d4e0d833861a678
Davi de Sousa Costa <davi.scosta@sempreceub.com>
2025-02-26 10:01:17 -0300

    Hello

---
+++ CODIGOC/Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, X;
+    
+    printf("Insira dois numeros inteiros: \n");
+    scanf("%i %i", &a, &b);
+
+    printf("A soma dos numeros eh: \nX = %i", X = a + b);
+}
