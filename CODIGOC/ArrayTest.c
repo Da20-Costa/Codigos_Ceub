6d6301a5802552b920dd74a9592f7d847f2a1178
Davi Costa <davi.scosta@sempreceub.com>
2025-03-12 21:06:05 -0300

    1-semestre-ceub

--- CODIGOC/ArrayTest.c
+++ CODIGOC/ArrayTest.c
@@ -2,8 +2,8 @@
 
 int main (void){
     int numbers [100];
-    for (int i = 0; i < 99; i++){
+    for (int i = 0; i < 100; i++){
         numbers [i] = i;
+        printf ("%i\n", numbers[i]);
     }
-    printf ("%i", numbers [99]);
 }
